ff541d26b5bf619cac69278ca3ed04c30ce8c973
Wagner Perin <wagnerperin@gmail.com>
2022-03-04 09:55:54 -0300

    Connecting server to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./conf/express');
 const config = require('./conf/config');
 
+const database = require('./conf/database');
+database.conect(config.DBURI);
+
 app.listen(config.PORT, config.HOST, ()=>{
     console.log(`App listen on http://${config.HOST}:${config.PORT}`);
 });
