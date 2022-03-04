62de94519e1720f02a7e8bb44164ce06da880f39
Wagner Perin <wagnerperin@gmail.com>
2022-03-04 09:55:20 -0300

    Setting up express autoload with consign and mongo

--- conf/config.js
+++ conf/config.js
@@ -3,5 +3,6 @@ const dotenv = require('dotenv').config();
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || 'localhost',
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    DBURI: process.env.DBURI || 'mongodb://127.0.0.1/CMPaaS'
 };
--- conf/express.js
+++ conf/express.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const consign = require('consign');
 
-app.get('/', (_, res) => {
-    res.send('Hello world');
-});
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+app.use(express.json());
+
+consign({cwd: 'app'})
+    .include('helpers')
+    .then('models')
+    .then('api')
+    .then('routes')
+    .into(app);
 
 module.exports = app;
