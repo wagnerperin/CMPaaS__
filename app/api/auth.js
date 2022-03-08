79f4de76bb8b8a87dc845aa7741f355ed0c6e1bf
Wagner Perin <wagnerperin@gmail.com>
2022-03-08 17:14:47 -0300

    Creating a authenticationOptional.

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,8 +1,24 @@
 module.exports = app => {
     const jwt = require('jsonwebtoken');
     const api = {};
-
     const userModel = require('mongoose').model('User');
+    
+    const loadToken = async (req) => {
+        req.isAuthenticated = () => req.user ? true : false;
+
+        const token = req.headers['x-access-token'];
+        if(!token) return {};
+        
+        return new Promise((resolve, reject) => {
+            jwt.verify(token, process.env.SECRET, (err, decoded) => {
+                if(err) reject({ error: "Failed to authenticate token." });
+                
+                req.user = { _id: decoded.id };
+                
+                resolve();
+            });
+        });
+    };
 
     api.authenticate = async (req, res) => {
         let user = await userModel.findOne({ email: req.body.email });
@@ -14,18 +30,19 @@ module.exports = app => {
         
     };
 
-    api.authenticationRequired = async (req, res, next) => {
-        const token = req.headers['x-access-token'];
-        if(!token) return res.status(401).json({ error: "Authentication required." });
-
-        jwt.verify(token, process.env.SECRET, (err, decoded) => {
-            if(err) return res.status(401).json({ error: "Failed to authenticate token." });
-            
-            req.user = { _id: decoded.id };
-            next();
-        })
+    api.authenticationOptional = async (req, res, next) => {
+        await loadToken(req).catch(err => res.status(401).json({ err }));
+        
+        next();
     };
 
+    api.authenticationRequired = async (req, res, next) => {
+        await loadToken(req).catch(err => res.status(401).json({ err }));
+
+        if(!req.isAuthenticated()) return res.status(401).json({ error: "Authentication required." });
+        
+        next();
+    };
 
     return api;
 }
